05d42c097fd41bcfeb48540add566e441f162c06
christophe <chrismuzikos>
2017-02-08 21:45:49 +0100

    add function to save state

--- src/components/Formulaire.js
+++ src/components/Formulaire.js
@@ -1,10 +1,28 @@
 import React from 'react';
 
 class Formulaire extends React.Component {
+
+    createMessage = event => {
+        event.preventDefault();
+        const message = this.message.value;
+        this.props.adMessage(message);
+        //reset
+        this.messageForm.reset();
+    };
+
     render() {
         return (
-            <form className="form">
-                <textarea required maxLength="140"></textarea>
+            <form
+                className="form"
+                onSubmit={e => this.createMessage(e)}
+                ref={input => this.messageForm = input}
+            >
+                <textarea
+                    required
+                    maxLength="140"
+                    ref={input => this.message = input}
+                >
+                    </textarea>
 
                 <div className="info">140</div>
 
